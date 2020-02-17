fd54fb99e6f505a9bb29cd69448ff77a84a9e188
Raymond Grumney <Raymond.Grumney@gmail.com>
2020-02-17 10:29:11 -0800

    Moved Sprite Exploder to CommonAssets projet

--- Assets/Scripts/Meta Scripts/MyUtilities.cs
+++ Assets/Scripts/Meta Scripts/MyUtilities.cs
@@ -30,28 +30,11 @@ public static class MyUtilities {
 	/// <returns>The angle in degrees.</returns>
 	/// <param name="a">The first Vector2</param>
 	/// <param name="b">The second Vector2</param>
-	public static float AngleInDegrees(Vector2 a, Vector2 b) {
-
+	public static float AngleInDegrees(Vector2 a, Vector2 b) 
+	{
 		Vector2 c = a - b;
-//		float angle =  ( Mathf.Atan2( c.y, c.x ) * Mathf.Rad2Deg ) - 90;
 		float angle =  ( Mathf.Atan2( c.y, c.x ) * Mathf.Rad2Deg );
-
-//		Debug.Log("MyUtilities.AngleInDegrees() angle: " + angle);
-//		Debug.Log("MyUtilities.AngleInDegrees() Vector2.Angle(a,b): " + Vector2.Angle(a,b));
-
-//		angle *= Mathf.Rad2Deg;
-//		angle *= Mathf.Rad2Deg * -2;
-//		angle *= Mathf.Rad2Deg * -2;
-
-		// do not want negative results
-//		if (angle < 0) {
-//			angle += 360;
-//		}
-
 		return angle + 180;
-
-
-
 	}
 
 	/// <summary>
@@ -60,7 +43,8 @@ public static class MyUtilities {
 	/// <returns>The angle in degrees.</returns>
 	/// <param name="a">The first Vector2</param>
 	/// <param name="b">The second Vector2</param>
-	public static float AngleInDegrees(Vector3 vector) {
+	public static float AngleInDegrees(Vector3 vector) 
+	{
 		return AngleInDegrees( (Vector2) vector );
 	}
 
@@ -70,7 +54,8 @@ public static class MyUtilities {
 	/// <returns>The angle in degrees.</returns>
 	/// <param name="a">The first Vector2</param>
 	/// <param name="b">The second Vector2</param>
-	public static float AngleInDegrees(Vector2 vector) {
+	public static float AngleInDegrees(Vector2 vector) 
+	{
 		return AngleInDegrees( Vector2.zero, vector );
 	}
 
@@ -80,7 +65,8 @@ public static class MyUtilities {
 	/// <returns>The angle in degrees.</returns>
 	/// <param name="a">The first Vector2</param>
 	/// <param name="b">The second Vector2</param>
-	public static float AngleInDegrees(Vector3 a, Vector3 b) {
+	public static float AngleInDegrees(Vector3 a, Vector3 b) 
+	{
 		return AngleInDegrees( (Vector2) a, (Vector2) b );
 	}
 
@@ -91,7 +77,8 @@ public static class MyUtilities {
 	/// <returns>The angle in degrees.</returns>
 	/// <param name="a">The first Vector2</param>
 	/// <param name="b">The second Vector2</param>
-	public static float AngleInDegrees(Vector2 a, Vector3 b) {
+	public static float AngleInDegrees(Vector2 a, Vector3 b) 
+	{
 		return AngleInDegrees( a, (Vector2) b );
 	}
 
@@ -102,7 +89,8 @@ public static class MyUtilities {
 	/// <returns>The angle in degrees.</returns>
 	/// <param name="a">The first Vector2</param>
 	/// <param name="b">The second Vector2</param>
-	public static float AngleInDegrees(Vector3 a, Vector2 b) {
+	public static float AngleInDegrees(Vector3 a, Vector2 b) 
+	{
 		return AngleInDegrees( (Vector2) a, b );
 	}
 
@@ -110,10 +98,17 @@ public static class MyUtilities {
 	/// Calculates the chance per delta time.
 	/// </summary>
 	/// <returns>The chance per delta time.</returns>
-	public static bool CalcChancePerDeltaTime( float chance ) {
-		float r = Random.Range( 0f, 1f / chance );
-
-		return r < Time.deltaTime;
+	public static bool CalculateChancePerDeltaTime( float chance ) 
+	{
+		if (!IsNearEnough(chance, 0f, 0.000001f) )
+		{
+			float r = Random.Range(0f, 1 - chance);
+			return r < Time.deltaTime;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	/// <summary>
@@ -123,13 +118,8 @@ public static class MyUtilities {
 	/// <param name="a">The first vector.</param>
 	/// <param name="b">The second vector component.</param>
 	/// <param name="margin">Margin.</param>
-	public static bool IsNearEnough(Vector2 a, Vector2 b, float margin) {
-
-//		Debug.Log("a:                        " + a);
-//		Debug.Log("b:                        " + b);
-//		Debug.Log("Vector2.Distance( a, b ): " + Vector2.Distance( a, b ));
-
-
+	public static bool IsNearEnough(Vector2 a, Vector2 b, float margin) 
+	{
 		return Vector2.Distance( a, b ) < margin;
 	}
 
@@ -140,31 +130,9 @@ public static class MyUtilities {
 	/// <param name="a">The first vector.</param>
 	/// <param name="b">The second vector component.</param>
 	/// <param name="margin">Margin.</param>
-	public static bool IsNearEnough(float a, float b, float margin) {
-
-//		Debug.Log("a:                  " + a);
-//		Debug.Log("b:                  " + b);
-//		Debug.Log("Mathf.Abs( a - b ): " + Mathf.Abs( a - b ));
-
-
+	public static bool IsNearEnough(float a, float b, float margin) 
+	{
 		return Mathf.Abs( a - b ) < margin;
-	}
-
-	/// <summary>
-	/// Moves the passed GameObject
-	/// </summary>
-	/// <param name="gObj">Game object to move.</param>
-	/// <param name="speed">The Speed at which the object moves.</param>
-	/// <param name="acceleration">How fast the object accellerates.</param>
-	/// <param name="facing">the direction the object is moving.</param>
-	public static void Move( GameObject gObj, float maxSpeed, float acceleration, Vector2 facing ) {
-		Rigidbody2D rigidbody = gObj.GetComponent<Rigidbody2D>();
-
-		if( Mathf.Abs( rigidbody.velocity.x ) < maxSpeed ) {
-			
-			// add force forward
-			rigidbody.AddForce( facing * acceleration );
-		}
 	}
 
 	/// <summary>
@@ -172,7 +140,8 @@ public static class MyUtilities {
 	/// </summary>
 	/// <returns>The time into a standard M:SS:II format.</returns>
 	/// <param name="time">Time.</param>
-	public static string FormatTime( float time ) {
+	public static string FormatTime( float time ) 
+	{
 		int m = (int) (Mathf.Floor( time % 3600f ) / 60f);		// minutes (2 digits)
 
 		if (m > 9) {
@@ -210,8 +179,8 @@ public static class MyUtilities {
 	/// <param name="number">Some number.</param>
 	/// <param name="min">Minimum.</param>
 	/// <param name="max">Max.</param>
-	public static bool Between(float number, float min, float max) {
-
+	public static bool Between(float number, float min, float max)
+	{
 		// allows reversable numbers
 		if (max > min) {
 			return number >= min && number <= max;
@@ -229,8 +198,8 @@ public static class MyUtilities {
 	/// <param name="frequency">How many times per second to cycle through full oscillation.</param>
 	/// <param name="phase">The offset of the oscillation at time 0.</param>
 	/// <param name="time">Time.</param>
-	public static float Oscillation(float amplitude, float frequency, float phase, float time) {
-
+	public static float Oscillation(float amplitude, float frequency, float phase, float time) 
+	{
 		return amplitude * Mathf.Sin( angularFrequency( frequency ) * time + phase );
 	}
 
@@ -242,64 +211,14 @@ public static class MyUtilities {
 	/// <param name="frequency">How many times per second to cycle through full oscillation.</param>
 	/// <param name="phase">The offset of the oscillation at time 0.</param>
 	/// <param name="timeToDecay">How long it takes to decay.</param>
-	public static float DampedOscillation(float amplitude, float frequency, float phase, float time, float timeToDecay) {
+	public static float DampedOscillation(float amplitude, float frequency, float phase, float time, float timeToDecay) 
+	{
 		return amplitude * Mathf.Pow( e, timeToDecay * time ) * Mathf.Cos( angularFrequency( frequency ) * time + phase );
 	}
 
-	private static float angularFrequency( float frequency ) {
+	private static float angularFrequency( float frequency ) 
+	{
 		return 2 * Mathf.PI * frequency;
 	}
 
-	public static void BreakUp(Sprite sprite, int size, int pixelsPerUnit, Vector3 position, float fadeOutTime){
-
-		// Get sprite texture
-		Texture2D sourceTexture = sprite.texture; 
-		Vector2 pivot = sprite.pivot;
-		Rect rect = sprite.rect;
-		Debug.Log(sprite.texture.name);
-
-		// Get sprite width and height
-		int width = Mathf.FloorToInt( sprite.rect.width );
-		int height = Mathf.FloorToInt( sprite.rect.height );
-
-
-		int partNumber = 0;
-
-		for(int i = 0; i < width / size; i++) {
-			for(int j = 0; j < height / size; j++) {
-
-				// Cut out the needed part from the texture
-				Sprite newSprite = Sprite.Create(	sourceTexture, 
-					new Rect( rect.x + i * width / ( width / size ), rect.y + j * height / ( height / size ), size, size), 
-					new Vector2(0f, 0f),
-					pixelsPerUnit );
-
-				GameObject gObject = new GameObject();
-				SpriteRenderer sRenderer = gObject.AddComponent<SpriteRenderer>();
-				Rigidbody2D rBody = gObject.AddComponent<Rigidbody2D>();
-				FadeOut fOut = gObject.AddComponent<FadeOut>();
-
-				fOut.fadeOutTime = fadeOutTime;
-
-				gObject.name = sprite.name + " part " + partNumber;
-
-				sRenderer.sprite = newSprite;
-				sRenderer.color = Color.white;
-				sRenderer.sortingLayerName = "Local Displays";
-
-
-				// Place every GameObject as it was in the original sprite
-				gObject.transform.position = position + new Vector3(	sprite.bounds.min.x + ( sRenderer.sprite.rect.width / pixelsPerUnit ) * i, 
-					sprite.bounds.min.y + ( sRenderer.sprite.rect.width / pixelsPerUnit ) * j, 
-					0 );
-
-
-				rBody.velocity = new Vector2( Random.Range( -2f , 2f ), Random.Range( 3f, 6f ) );
-				rBody.freezeRotation = true;
-
-
-				partNumber++;
-			}
-		}
-	}
 }
