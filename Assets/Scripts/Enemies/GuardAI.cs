7aab0e593eb2f9dcc1319533ae2db4466505adce
Raymond Grumney <Raymond.Grumney@gmail.com>
2020-02-17 10:11:54 -0800

    Clean up; debug code

--- Assets/Scripts/Enemies/GuardAI.cs
+++ Assets/Scripts/Enemies/GuardAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using CommonAssets.Utilities;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,20 +16,22 @@ public class GuardAI : BaseEnemy {
 	public float idleChance;
 
 	[Tooltip("How often the AI makes the choice to wander, return home, or idle.")]
-	public float randomChoiceEveryNSeconds = 2;
+	public float randomChoiceEveryNSeconds = 2f;
 
 	[Tooltip("Likelihood the AI turns while idling.")]
-	public float turnChanceDuringIdle = 1;
+	[Range(0, 1)]
+	public float turnChanceDuringIdle = 1f;
 
 	[Tooltip("The angle of the cast while seeking a point to wander to.")]
 	[Range(0,1)]
-	public float wanderAngle = 0.7f;
+	public float MaxWanderAngle = 0.7f;
 
 	// when the last choice was made
 	protected float nextRandomChoice = 0;
 
 	// check if forgetten player 
-	protected override bool forgetBehavior(){
+	protected override bool ForgetBehavior()
+	{
 		// if the enemy has forgotten it saw the character, return to starting point
 		if (_lastSawCharacter < Time.time) {
 			_targetPoint = _startingPoint;
@@ -43,30 +46,39 @@ public class GuardAI : BaseEnemy {
 
 
 	// idle 
-	protected override void idleBehavior()
+	protected override void IdleBehavior()
 	{
-		if ( MyUtilities.CalcChancePerDeltaTime( turnChanceDuringIdle ) ) {
-			setFacing( -facing.x );
+
+		if ( MyUtilities.CalculateChancePerDeltaTime( turnChanceDuringIdle ) ) 
+		{
+			SetFacing( -facing.x );
 		}
 	}
 
 
-	protected override void defaultBehavior(){
-
-		if( !_pursuing ) {
+	protected override void DefaultBehavior()
+	{
+		if( !_pursuing ) 
+		{
 			// if target has been set to starting point
-			if (nextRandomChoice < Time.time){
+			if (nextRandomChoice < Time.time)
+			{
 				// if it's time to make another random choice
-				if ( _collider.OverlapPoint(_gotoPoint ) ) {
+				if (_collider.OverlapPoint(new Vector2(_gotoPoint.x, transform.position.y))) // don't care about height in level (since he can't jump)
+				{
 					// we're currently not heading toward a position
-					makeRandomChoice();
-				} else {
+					MakeRandomChoice();
+				} 
+				else 
+				{
 					// otherwise perform the last choice
-					performLastRandomChoice();
+					PerformLastRandomChoice();
 				}
-			} else {
+			} 
+			else 
+			{
 				// execute whatever the last choice was
-				performLastRandomChoice();
+				PerformLastRandomChoice();
 			}
 		}
 	}
@@ -74,8 +86,9 @@ public class GuardAI : BaseEnemy {
 	/// <summary>
 	/// Makes a random choice.
 	/// </summary>
-	protected void makeRandomChoice()	
+	protected void MakeRandomChoice()	
 	{
+		Debug.Log("MakeRandomChoice");
 		// set random choice timer
 		nextRandomChoice = Time.time + randomChoiceEveryNSeconds;
 
@@ -84,14 +97,19 @@ public class GuardAI : BaseEnemy {
 		// generate a random number
 		float r = Random.Range( 0f, choices );
 
-		if (r < wanderChance) {
+		if (r < wanderChance) 
+		{
 			// wander from starting point
-			chooseWanderLocation();
-		} else if (r < wanderChance + returnToHomeChance) {
+			ChooseWanderLocation();
+		} 
+		else if (r < wanderChance + returnToHomeChance) 
+		{
 			// go back to starting point
 			_gotoPoint = _startingPoint;
-			setFacing( Mathf.Sign( _startingPoint.x - transform.position.x ) );
-		} else {
+			SetFacing( Mathf.Sign( _startingPoint.x - transform.position.x ) );
+		} 
+		else 
+		{
 			// idle 
 			_gotoPoint = transform.position;
 		}
@@ -101,65 +119,72 @@ public class GuardAI : BaseEnemy {
 	/// <summary>
 	/// Chooses the wander location.
 	/// </summary>
-	protected void chooseWanderLocation()
+	protected void ChooseWanderLocation()
 	{
-		// a random downward angle
-		Vector2 downwardAngle = Vector2.down * Random.Range( wanderAngle * 0.1f, wanderAngle );
-		// a point in front of the enemy
-		RaycastHit2D hit = Physics2D.Raycast( transform.position, facing + downwardAngle, 5f, _physicsLayer );
-		// the current y level of the floor
-		float floorLevel = Physics2D.Raycast( transform.position, Vector2.down, 5f, _physicsLayer ).point.y;
+
+		Debug.Log("ChooseWanderLocation");
+		Vector2 randomDownwardAngle = Vector2.down + ( facing * Random.Range( MaxWanderAngle * 0.1f, MaxWanderAngle ) );
+		RaycastHit2D pointInFrontOfThis = Physics2D.Raycast( transform.position, randomDownwardAngle, 10f, _physicsLayer );
+		Easily.Instantiate(spoon, pointInFrontOfThis.point);
+
+		float floorYLevel = Physics2D.Raycast( transform.position, Vector2.down, 5f, _physicsLayer ).point.y;
 
 		// if the difference in height between the chosen point and the current floor is neglegible
-		if ( Mathf.Abs( hit.point.y - floorLevel ) < 0.1f) {
+		if ( Mathf.Abs( pointInFrontOfThis.point.y - floorYLevel ) < 0.1f) 
+		{
 			// set _wanderPoint to hit position
-			_gotoPoint = new Vector2( hit.point.x, transform.position.y );
-			//setFacing( _facing.x );
-		} else {
+			_gotoPoint = new Vector2( pointInFrontOfThis.point.x, transform.position.y );
+		} 
+		else 
+		{
 			// make another choice 
 			nextRandomChoice = 0;
 		}
 	}
 
+	public GameObject spoon;
+
 	/// <summary>
 	/// Performs the last random choice.
 	/// </summary>
-	protected void performLastRandomChoice()
+	protected void PerformLastRandomChoice()
 	{
-
-		if ( ! isNearEnough( transform.position.x, _gotoPoint.x ) ) {
+		Debug.Log("PerformLastRandomChoice");
+		if ( ! isNearEnough( transform.position.x, _gotoPoint.x ) ) 
+		{
 			// wander / return home
-
 			// this should determine if we've passed the target point
-			if ( ! _collider.OverlapPoint( _gotoPoint ) ) {
+			if ( ! _collider.OverlapPoint( _gotoPoint ) ) 
+			{
 				moveForward();
-			} else {
+			} 
+			else 
+			{
 				_gotoPoint = transform.position;
 			}
 
 		} else {
 			// we're at the goto point and/or idling
-			idleBehavior();
+			IdleBehavior();
 		}
 	}
 
 	// pursuit behavior
-	protected override void pursuitBehavior() {
-
+	protected override void PursuitBehavior() 
+	{
 		// if we're not near the target point
-		if ( ! _collider.OverlapPoint( _targetPoint ) ) {
-
-			//Vector2 vector = Vector2.Lerp( transform.position, _targetPoint, 1 );
-
-			// set facing to the direction the target is 
+		if ( ! _collider.OverlapPoint( _targetPoint ) ) 
+		{
 			faceTarget();
 
 			// see if there's a surface immediately ahead of us to walk on
 			RaycastHit2D hit = Physics2D.Raycast( this.transform.position, facing + Vector2.down, 1f, _physicsLayer );
 
 			// if the target isn't above us (which would it even be?) 
-			if ( hit != null ) {
-				if( ! isNearEnough( hit.point.y, this.transform.position.y ) ) {
+			if ( hit ) 
+			{
+				if( ! isNearEnough( hit.point.y, this.transform.position.y ) ) 
+				{
 					moveForward();
 				}
 			}
