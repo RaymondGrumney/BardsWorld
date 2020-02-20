6fe56ebf0cd89c26ec88459987504db78edd4f7c
Raymond Grumney <Raymond.Grumney@gmail.com>
2020-02-19 19:40:37 -0800

    Added Player Ducking/Crouching

--- Assets/Scripts/Player Controllers/Character.cs
+++ Assets/Scripts/Player Controllers/Character.cs
@@ -12,13 +12,18 @@ public class Character : MonoBehaviour
 	/// </summary>
 	[Tooltip("Whether this character is active.")]
 	public bool isActive = false;
+	/// <summary>
+	/// Sets whether the character is active.
+	/// </summary>
+	/// <param name="state">True or False</param>
+	/// <returns></returns>
 	public bool SetActive(bool state = true) { return isActive = state; }
 
 	/// <summary>
 	/// Whether the character is swimming.
 	/// </summary>
 	/// <value><c>true</c> if is swimin; otherwise, <c>false</c>.</value>
-	public bool isSwimming { set; get; }
+	public bool IsSwimming { set; get; }
 	
 
 	[Header("Movement")]
@@ -27,6 +32,9 @@ public class Character : MonoBehaviour
 	/// </summary>
 	[Tooltip("This character's maximum speed.")]
 	public float maxSpeed = 3;
+	/// <summary>
+	/// This character's maximum speed.
+	/// </summary>
 	public float horizontal;
 
 
@@ -35,31 +43,18 @@ public class Character : MonoBehaviour
 	/// If the character is currently receiving input. Turn on / off to control if the character is receiving input
 	/// </summary>
 	[Tooltip("Whether the character is currently receiving input.")]
-	private bool _receivingInput = true;
-
-	public bool receivingInput{
-		get{ return _receivingInput; }
-		set{ _receivingInput = value; }
-	}
+	public bool ReceivingInput = true;
 
 	/// <summary>
-	/// The next Time.time players can control this object
+	/// The how long it's been sine last input
 	/// </summary>
-	private float _idleSince = 0;					// counts length of time since last input
+	private float _idleSince = 0;
 
 
 	/// <summary>
 	/// Whether the caracter can currently move. Adjust this in other scripts to prevent character movement.
 	/// </summary>
-	private bool _canMove = true;	
-
-	/// <summary>
-	/// Whether the caracter is currently allowed to move.
-	/// </summary>
-	public bool CanMove{
-		get => _canMove;
-		set => _canMove = value; 
-	}
+	public bool CanMove = true;	
 
 
 	/// <summary>
@@ -81,7 +76,7 @@ public class Character : MonoBehaviour
 	/// <summary>
 	/// the normalized the character is "facing".
 	/// </summary>
-	protected Vector2 _facing = Vector3.right;
+	protected Vector2 _facing = Vector2.right;
 
 	/// <summary>
 	/// The normalized direction this character is "Facing."
@@ -114,10 +109,10 @@ public class Character : MonoBehaviour
 	void FixedUpdate()
 	{
 		if ( isActive ) {
-			standardUpdateActions();
+			StandardUpdateActions();
 
-			if ( _receivingInput ) {
-				checkMovement();
+			if ( ReceivingInput ) {
+				CheckMovement();
 			}
 		} 
 	}
@@ -133,76 +128,58 @@ public class Character : MonoBehaviour
 	}
 
 	// Actions 
-	protected void standardUpdateActions() {
+	protected void StandardUpdateActions() 
+	{
 		horizontal = Joypad.Read.Buttons.horizontal;
 	}
 
 
 	// move based on player movement
-	private void checkMovement(){
-		
-		// start with _moveVector and current y velocity
-//		Vector2 moveSpeed = _moveVector;
-		Vector2 moveSpeed = Vector2.zero;
-
-		//moveSpeed.y += _rigidbody.velocity.y;
-
-		if (inputCheck() && _canMove) {
-			
+	private void CheckMovement()
+	{
+		if (InputCheck() && CanMove) 
+		{
 			float h = Joypad.Read.Buttons.horizontal;
 
 			// if the value on the horizontal axis is +/- 0.1
-			if (Mathf.Abs( h ) > 0.1f)
+			if ( Mathf.Abs( h ) > 0.1f )
 			{
-				// move the character
-				_rigidbody.velocity = new Vector2(h * maxSpeed, _rigidbody.velocity.y);
-
-				// check if new facing and rotate
 				Vector2 newFacing = new Vector2(Mathf.Ceil(h), 0);
-
 				if (_facing != newFacing)
 				{
 					_facing = newFacing;
 					Easily.Flip(gameObject).On("yAxis");
 				}
 			}
-			else {
-				// stop horizontal movement if the player is not moving the character
-//				stop();
-			}
 
-			// set Params of animator
-			_animator.SetFloat( "Speed", Mathf.Abs( h ) );
+			if (!Ducking())
+			{
+				_rigidbody.velocity = new Vector2(h * maxSpeed, _rigidbody.velocity.y);
+				_animator.SetFloat("Speed", Mathf.Abs(h));
+			}
 		} else {
 			// set Params of animator
 			_animator.SetFloat( "Speed", 0 );
 		}
 	}
 
-
-	// checks current acctions (currently this is handled by individual scripts
-	private void checkActions() {
-		
-	}
-
 	/// <summary>
 	/// If this character is currently receiving input
 	/// </summary>
 	/// <returns><c>true</c>, if input is being accepted, <c>false</c> otherwise.</returns>
-	public bool inputCheck()
+	public bool InputCheck()
 		// it is currently receiving input if:
 		//   it's marked active
 		//   it's marked as receiving input (obviously)
 		//   it's time out value is in the past
-		=> isActive && _receivingInput;
-		//return isActive && _receivingInput && _inputTimeOut < Time.time;
+		=> isActive && ReceivingInput;
 
 
 
 	/// <summary>
 	/// Stops movement for this character
 	/// </summary>
-	public virtual void stop()
+	public virtual void Stop()
 	{
 		if (_grounded) 
 		{
@@ -216,7 +193,7 @@ public class Character : MonoBehaviour
 	/// Gets or sets a value indicating whether this <see cref="Character"/> is grounded.
 	/// </summary>
 	/// <value><c>true</c> if grounded; otherwise, <c>false</c>.</value>
-	public bool grounded {
+	public bool Grounded {
 		get{ return _grounded; }
 		set {
 			_grounded = value;
@@ -224,5 +201,22 @@ public class Character : MonoBehaviour
 			// also let the animator know the character is grounded.
 			_animator.SetBool( "Grounded", value );
 		}
+	}
+
+	public bool Ducking() 
+	{
+		if ( Joypad.Read.Buttons.Held("down") )
+		{
+			_animator.SetBool("Ducking", true);
+			return true;
+		}
+
+		if ( !Joypad.Read.Buttons.Held("down") )
+		{
+			_animator.SetBool("Ducking", false);
+			return false;
+		}
+
+		return Joypad.Read.Buttons.Held("down");
 	}
 }
