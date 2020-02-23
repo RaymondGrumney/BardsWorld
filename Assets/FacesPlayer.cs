756ff4fccc09a891748a2003a7cabf90d243a51d
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-23 11:44:02 -0800

    Simple script to flip object when Character passes it on X Axis

---
+++ Assets/FacesPlayer.cs
@@ -0,0 +1,31 @@
+﻿using CommonAssets.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacesPlayer : MonoBehaviour
+{
+    private GameObject player;
+    private bool flipped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Bard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > player.transform.position.x && !flipped)
+        {
+            flipped = true;
+            Easily.Flip(gameObject).On("YAxis");
+        }
+        else if (transform.position.x < player.transform.position.x && flipped)
+        {
+            flipped = false;
+            Easily.Flip(gameObject).On("YAxis");
+        }
+    }
+}
