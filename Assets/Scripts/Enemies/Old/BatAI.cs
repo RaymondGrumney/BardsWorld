ca77b43b25f28a5f8a389b6398caaaf828a14739
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-23 20:05:24 -0800

    Archived old Bat

--- Assets/Scripts/Enemies/Old/BatAI.cs
+++ Assets/Scripts/Enemies/Old/BatAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BatAI : BaseEnemyAI {
+public class BatAIOld : BaseEnemyAI {
 
 	[Header("Bat AI Settings")]
 	[Tooltip("How high we fly.")]
