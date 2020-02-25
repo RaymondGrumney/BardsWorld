ca1d0710df96ce4deb6640a5e06d4f476c2d75e1
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-24 21:21:09 -0800

    Added Tags for impacting

--- Assets/Scripts/Objects/Events/DestroyOnImpact.cs
+++ Assets/Scripts/Objects/Events/DestroyOnImpact.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class DestroyOnImpact : MonoBehaviour {
 
-	public float forceToDestroy;
-	private Rigidbody2D _rigidbody;
+	public List<string> DestroyOnTheseTags = new List<string>();
 
-	void Start() {
-		startRoutine();
-	}
-
-	void startRoutine() {
-		_rigidbody = this.GetComponent<Rigidbody2D>();
-	}
-
-	void OnCollisionEnter2D(Collision2D other) {
-		// TODO: account for bounciness on this and other's physics material
-
-		Debug.Log( "Plink : " + _rigidbody.gameObject.name);
-		Debug.Log( "Plink : x = " + _rigidbody.velocity.x );
-		Debug.Log( "Plink : y = " + _rigidbody.velocity.y );
-		Debug.Log( "Plink : mass =" + _rigidbody.mass ) ;
-		Debug.Log( "Plink : result = " + ( Mathf.Abs( ( _rigidbody.velocity.x + _rigidbody.velocity.y) * _rigidbody.mass ) ) );
-
-		if( Mathf.Abs ( this._rigidbody.velocity.x ) + Mathf.Abs( this._rigidbody.velocity.y) * this._rigidbody.mass > forceToDestroy ) {
-			Destroy( this.gameObject );
+	void OnCollisionEnter2D(Collision2D other) 
+	{
+		if(DestroyOnTheseTags.Count == 0 || DestroyOnTheseTags.Contains( other.gameObject.tag )) 
+		{
+			StartCoroutine(Destroy());
 		}
+	}
+
+	public IEnumerator Destroy()
+	{
+		yield return new WaitForEndOfFrame();
+		Destroy(this.gameObject);
 	}
 }
