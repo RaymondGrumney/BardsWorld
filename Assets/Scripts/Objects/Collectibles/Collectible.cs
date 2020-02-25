3dd8c27696ee6df7318f7f2c929e732723752c8a
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-25 12:49:12 -0800

    No longer Abstract; Sends Collect Message to other Scripts

--- Assets/Scripts/Objects/Collectibles/Collectible.cs
+++ Assets/Scripts/Objects/Collectibles/Collectible.cs
@@ -6,31 +6,24 @@ using UnityEngine;
 /// <summary>
 /// A collectible object. 
 /// </summary>
-public abstract class Collectible : MonoBehaviour {
+public class Collectible : MonoBehaviour {
 
 	[Tooltip("The sound affect on pick up.")]
-	/// <summary>
-	/// The sound affect on pick up.
-	/// </summary>
 	public AudioClip onPickupSFX;
 
-	void OnTriggerEnter2D(Collider2D other) {
-		
-		// call the child object's Collect script
-		collect( other.gameObject );
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if(other.CompareTag("Player"))
+		{
+			SendMessage("Collect", other.gameObject);
 
-		// play the pick up sound
-		Easily.PlaySound(onPickupSFX);
+			// play the pick up sound
+			Easily.PlaySound(onPickupSFX);
 
-		// turn off things
-		this.GetComponent<SpriteRenderer>().enabled = false;
-		this.GetComponent<Collider2D>().enabled = false;
-		this.enabled = false;
+			// turn off things
+			GetComponent<SpriteRenderer>().enabled = false;
+			GetComponent<Collider2D>().enabled = false;
+			enabled = false;
+		}
 	}
-
-	/// <summary>
-	/// The effect of picking up this object
-	/// </summary>
-	/// <param name="other">The object picking up this item.</param>
-	protected abstract void collect( GameObject other );
 }
