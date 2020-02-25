a6d8d3fc37667266ad6f99324c1ed2535ea4f95c
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-24 21:14:14 -0800

    Create Projectile.cs

---
+++ Assets/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Velocity = DefaultValues.MediumProjectileVelocity;
+
+    void Awake()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Velocity, 0);
+    }
+
+    public void SetRotation(Quaternion rotation)
+    {
+        transform.rotation = rotation;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-transform.rotation.y, transform.rotation.x) * Velocity;
+    }
+}
