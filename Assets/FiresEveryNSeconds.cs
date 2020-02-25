300899461e8195d16ab691439fdf5cb1358748d0
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-24 21:16:15 -0800

    ProtoType Turret

---
+++ Assets/FiresEveryNSeconds.cs
@@ -0,0 +1,33 @@
+﻿using CommonAssets.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiresEveryNSeconds : MonoBehaviour
+{
+    public GameObject Projectile;
+    public GameObject SpawnPoint;
+    public float FireFreqeuncy = 3f;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(Fire());
+    }
+
+    private IEnumerator Fire()
+    {
+        yield return new WaitForSecondsRealtime(FireFreqeuncy);
+
+        GameObject projectile = Easily.Instantiate(Projectile, SpawnPoint.transform.position);
+        projectile.SendMessage("SetRotation", Easily.Clone(gameObject.transform.rotation));
+
+        StartCoroutine(Fire());
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
