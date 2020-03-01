d50c72e9329d690fb2e5d57e7952ad5a9910ca5b
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-29 20:27:04 -0800

    Sets Rotation based on Velocity

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float Velocity = DefaultValues.MediumProjectileVelocity;
+    private Rigidbody2D _rigidbody;
 
     void Awake()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(Velocity, 0);
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = new Vector2(Velocity, 0);
+    }
+
+    public void Update()
+    {
+        Vector2 v = _rigidbody.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     public void SetRotation(Quaternion rotation)
     {
-        transform.rotation = rotation;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(-transform.rotation.y, transform.rotation.x) * Velocity;
-    }
+        transform.rotation = rotation;       
+        // TODO: do velocity correctly
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-transform.rotation.y * 2 + 1, transform.rotation.x ) * Velocity;
+     }
 }
